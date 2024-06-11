6cb23e4530b08bc6502034bd009f8e0b9b8ba2a2
richa <richa@LAPTOP-I1P2JR4I>
2024-06-10 19:42:44 -0500

    c

--- SistemaInvernaderoView/login.h
+++ SistemaInvernaderoView/login.h
@@ -39,9 +39,10 @@ namespace SistemaInvernaderoView {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox2;
 	protected:
 
 	private:
@@ -60,10 +61,13 @@ namespace SistemaInvernaderoView {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
 			this->label1->AutoSize = true;
 			this->label1->Location = System::Drawing::Point(80, 203);
 			this->label1->Name = L"label1";
@@ -71,6 +75,9 @@ namespace SistemaInvernaderoView {
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Usuario:";
 			this->label1->Click += gcnew System::EventHandler(this, &login::label1_Click);
+			// 
+			// label2
+			// 
 			this->label2->AutoSize = true;
 			this->label2->Location = System::Drawing::Point(80, 293);
 			this->label2->Name = L"label2";
@@ -78,15 +85,17 @@ namespace SistemaInvernaderoView {
 			this->label2->TabIndex = 0;
 			this->label2->Text = L"Contraseña:";
 			this->label2->Click += gcnew System::EventHandler(this, &login::label1_Click);
+			// 
+			// textBox1
+			// 
 			this->textBox1->Location = System::Drawing::Point(228, 203);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(192, 31);
 			this->textBox1->TabIndex = 1;
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &login::textBox1_TextChanged);
-			this->textBox2->Location = System::Drawing::Point(228, 293);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(192, 31);
-			this->textBox2->TabIndex = 1;
+			// 
+			// label3
+			// 
 			this->label3->AutoSize = true;
 			this->label3->Location = System::Drawing::Point(240, 85);
 			this->label3->Name = L"label3";
@@ -94,6 +103,9 @@ namespace SistemaInvernaderoView {
 			this->label3->TabIndex = 0;
 			this->label3->Text = L"Iniciar Sesión";
 			this->label3->Click += gcnew System::EventHandler(this, &login::label1_Click);
+			// 
+			// button1
+			// 
 			this->button1->Location = System::Drawing::Point(228, 391);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(153, 46);
@@ -101,17 +113,29 @@ namespace SistemaInvernaderoView {
 			this->button1->Text = L"Ingresar";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &login::button1_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(228, 293);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->PasswordChar = '*';
+			this->textBox2->Size = System::Drawing::Size(192, 31);
+			this->textBox2->TabIndex = 3;
+			// 
+			// login
+			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(617, 529);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label1);
 			this->Name = L"login";
 			this->Text = L"login";
+			this->Load += gcnew System::EventHandler(this, &login::login_Load_1);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -147,6 +171,10 @@ private: System::Void textBox2_TextChanged(System::Object^ sender, System::Event
 private: System::Void maskedTextBox1_MaskInputRejected(System::Object^ sender, System::Windows::Forms::MaskInputRejectedEventArgs^ e) {
 }
 private: System::Void login_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void login_Load_1(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox2_TextChanged_1(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
