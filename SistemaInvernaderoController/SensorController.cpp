1a3fb7266bc2bdaf3ec08fc35a7f6a2b2f12c540
Ines11 <a20192545@pucp.edu.pe>
2024-06-20 18:32:09 -0500

    Richard no me cae

--- SistemaInvernaderoController/SensorController.cpp
+++ SistemaInvernaderoController/SensorController.cpp
@@ -1,8 +1,5 @@
 #include "SensorController.h"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 using namespace SistemaInvernaderoController;
 using namespace System::IO;
 
@@ -28,24 +25,7 @@ List<sensor^>^ SensorController::buscarSensorxTipo(String^ tipoSensor) {
 	}
 	return listaSensor;
 }
-List < sensor^>^ SensorController::buscarSensorAll() {
-	List<sensor^>^ listaSensor = gcnew List<sensor^>();
-	array<String^>^ lineas = File::ReadAllLines("Sensor.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaSensor in lineas) {
-		array<String^>^ datos = lineaSensor->Split(separadores->ToCharArray());
-		int codigo = Convert::ToInt32(datos[0]);
-		String^ tipo = datos[1];
-		int enFuncionamiento = Convert::ToInt32(datos[2]);
-		double intervaloDeMuestreo = Convert::ToDouble(datos[3]);
-		double medicion = Convert::ToDouble(datos[4]);
-		String^ unidad = datos[5];
-		sensor^ objSensor = gcnew sensor(codigo, tipo, enFuncionamiento, intervaloDeMuestreo, medicion, unidad);
-		listaSensor->Add(objSensor);
-		
-	}
-	return listaSensor;
-}
+
 void SensorController::agregarNuevoSensor(int codigo, String^ tipo, int enFuncionamiento, double intervaloDeMuestreo, double medicion, String^ unidad) {
 	List < sensor^>^ listaSensor = buscarSensorAll();
 	sensor^ objSensorNuevo = gcnew sensor(codigo, tipo, enFuncionamiento, intervaloDeMuestreo, medicion, unidad);
@@ -105,117 +85,4 @@ void SensorController::actualizarSensor(int codigo, String^ tipo, int enFunciona
 		}
 	}
 	escribirArchivo(listaSensor);
-
-=======
-=======
-
->>>>>>> 7c42f2d (queso)
-using namespace SistemaInvernaderoController;
-using namespace System::IO;
-
-SensorController::SensorController() {
-
-}
-List<sensor^>^ SensorController::buscarSensorxTipo(String^ tipoSensor) {
-	List<sensor^>^ listaSensor = gcnew List<sensor^>();
-	array<String^>^ lineas = File::ReadAllLines("Sensor.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaSensor in lineas) {
-		array<String^>^ datos = lineaSensor->Split(separadores->ToCharArray());
-		int codigo = Convert::ToInt32(datos[0]);
-		String^ tipo = datos[1];
-		int enFuncionamiento = Convert::ToInt32(datos[2]);
-		double intervaloDeMuestreo = Convert::ToDouble(datos[3]);
-		double medicion = Convert::ToDouble(datos[4]);
-		String^ unidad = datos[5];
-		if (tipo->CompareTo(tipoSensor) == 0) {
-			sensor^ objSensor = gcnew sensor(codigo, tipo, enFuncionamiento, intervaloDeMuestreo, medicion, unidad);
-			listaSensor->Add(objSensor);
-		}
-	}
-	return listaSensor;
-}
-List < sensor^>^ SensorController::buscarSensorAll() {
-	List<sensor^>^ listaSensor = gcnew List<sensor^>();
-	array<String^>^ lineas = File::ReadAllLines("Sensor.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaSensor in lineas) {
-		array<String^>^ datos = lineaSensor->Split(separadores->ToCharArray());
-		int codigo = Convert::ToInt32(datos[0]);
-		String^ tipo = datos[1];
-		int enFuncionamiento = Convert::ToInt32(datos[2]);
-		double intervaloDeMuestreo = Convert::ToDouble(datos[3]);
-		double medicion = Convert::ToDouble(datos[4]);
-		String^ unidad = datos[5];
-		sensor^ objSensor = gcnew sensor(codigo, tipo, enFuncionamiento, intervaloDeMuestreo, medicion, unidad);
-		listaSensor->Add(objSensor);
-		
-	}
-	return listaSensor;
-}
-void SensorController::agregarNuevoSensor(int codigo, String^ tipo, int enFuncionamiento, double intervaloDeMuestreo, double medicion, String^ unidad) {
-	List < sensor^>^ listaSensor = buscarSensorAll();
-	sensor^ objSensorNuevo = gcnew sensor(codigo, tipo, enFuncionamiento, intervaloDeMuestreo, medicion, unidad);
-	listaSensor->Add(objSensorNuevo);
-	escribirArchivo(listaSensor);
-}
-void SensorController::escribirArchivo(List<sensor^>^ listaSensor) {
-	array<String^>^ lineasArchivo = gcnew array<String^>(listaSensor->Count);
-	for (int i = 0; i < listaSensor->Count; i++) {
-		sensor^ objSensor = listaSensor[i];
-		lineasArchivo[i] = Convert::ToString(objSensor->getcodigo()) + ";" + objSensor->gettipo() + ";" + objSensor->getenFuncionamiento() + ";" + Convert::ToString(objSensor->getintervaloDeMuestreo()) + ";" + Convert::ToString(objSensor->getmedicion()) + ";" + objSensor->getunidad();
-	}
-	File::WriteAllLines("Sensor.txt", lineasArchivo);
-
-}
-void SensorController::eliminarSensor(int codigo) {
-	List<sensor^>^ listaSensor = buscarSensorAll();
-	for (int i = 0; i < listaSensor->Count; i++) {
-		if (listaSensor[i]->getcodigo() == codigo) {
-			listaSensor->RemoveAt(i);
-			break;
-		}
-	}
-	escribirArchivo(listaSensor);
-}
-sensor^ SensorController::buscarSensorxCodigo(int codigoBuscado) {
-	sensor^ objSensor;
-	array<String^>^ lineas = File::ReadAllLines("Sensor.txt");
-	String^ separadores = ";";
-	for each (String ^ lineaSensor in lineas) {
-		array<String^>^ datos = lineaSensor->Split(separadores->ToCharArray());
-		int codigo = Convert::ToInt32(datos[0]);
-		String^ tipo = datos[1];
-		int enFuncionamiento = Convert::ToInt32(datos[2]);
-		double intervaloDeMuestreo = Convert::ToDouble(datos[3]);
-		double medicion = Convert::ToDouble(datos[4]);
-		String^ unidad = datos[5];
-		if (codigo == codigoBuscado) {
-			objSensor = gcnew sensor(codigo, tipo, enFuncionamiento, intervaloDeMuestreo, medicion, unidad);
-			break;
-		}
-
-	}
-	return objSensor;
-}
-
-void SensorController::actualizarSensor(int codigo, String^ tipo, int enFuncionamiento, double intervaloDeMuestreo, double medicion, String^ unidad) {
-	List<sensor^>^ listaSensor = buscarSensorAll();
-	for (int i = 0; i < listaSensor->Count; i++) {
-		if (listaSensor[i]->getcodigo() == codigo) {
-			listaSensor[i]->settipo(tipo);
-			listaSensor[i]->setenFuncionamiento(enFuncionamiento);
-			listaSensor[i]->setintervaloDeMuestreo(intervaloDeMuestreo);
-			listaSensor[i]->setmedicion(medicion);
-			listaSensor[i]->setunidad(unidad);
-			break;
-		}
-	}
-	escribirArchivo(listaSensor);
-
-<<<<<<< HEAD
-	
->>>>>>> 5fa86e0 (Agregar archivos de proyecto.)
-=======
->>>>>>> 7c42f2d (queso)
 }
--- SistemaInvernaderoController/SensorController.h
+++ SistemaInvernaderoController/SensorController.h
@@ -1,8 +1,6 @@
 #pragma once
 
 namespace SistemaInvernaderoController {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 	using namespace System::Collections::Generic;
 	using namespace SistemaInvernaderoModel;
@@ -18,28 +16,5 @@ namespace SistemaInvernaderoController {
 			void escribirArchivo(List<sensor^>^ listaSensor);
 			void eliminarSensor(int codigo);
 			void actualizarSensor(int codigo, String^ tipo, int enFuncionamiento, double intervaloDeMuestreo, double medicion, String^ unidad);
-=======
-	public ref class SensorController {
-	public:
-		SensorController();
-
->>>>>>> 5fa86e0 (Agregar archivos de proyecto.)
-=======
-
-	using namespace System::Collections::Generic;
-	using namespace SistemaInvernaderoModel;
-	using namespace System;
-
-	public ref class SensorController {
-		public:
-			SensorController();
-			List < sensor^>^ buscarSensorxTipo(String^ tipoSensor);
-			List < sensor^>^ buscarSensorAll();
-			sensor^ buscarSensorxCodigo(int codigo);
-			void agregarNuevoSensor(int codigo, String^ tipo, int enFuncionamiento, double intervaloDeMuestreo, double medicion, String^ unidad);
-			void escribirArchivo(List<sensor^>^ listaSensor);
-			void eliminarSensor(int codigo);
-			void actualizarSensor(int codigo, String^ tipo, int enFuncionamiento, double intervaloDeMuestreo, double medicion, String^ unidad);
->>>>>>> 7c42f2d (queso)
 	};
 }
